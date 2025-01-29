3fe45a44407f1ba715c34c490cc26fe7a234dc2f
Abhijith <107354234+AbhijithIppa@users.noreply.github.com>
2025-01-29 09:11:23 +0000

    ypdated

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const LoginPage = ({ login, isLoggingIn }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
-  const [test2,setTest]=useState("");
+  const [test3,setTest]=useState("");
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
